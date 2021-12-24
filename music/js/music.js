db0829d764d748a1aa8b41011f3d21e571eac953
zh-liang <1185690533@qq.com>
2021-12-24 15:24:32 +0800

    :sparkles: 增加music

--- music/js/music.js
+++ music/js/music.js
@@ -5,23 +5,31 @@ const player = new APlayer({
     autoplay: true,
     lrcType: false,//
     mutex: true,
-    theme: 'rgb(194,12,12)',
+    theme: '#C20C0C',
     order: 'list',
     listMaxHeight: '600px',
     audio: [
         {
-            name: '大头皮鞋',
-            artist: '韩晓',
-            url: 'http://m10.music.126.net/20211220192538/0e62e47fad508457f42e42e81fd16c57/ymusic/obj/w5zDlMODwrDDiGjCn8Ky/3047811746/ecdb/b511/5061/648e19e0bdf045c4c559acb24ad2441d.mp3',
-            cover: 'https://p2.music.126.net/VIjxW7mevNwMbWQLR_iYIw==/111050674406169.jpg?param=90y90',
-            theme: '#468b66'
-        },
-        {
             name: '2002年的第一场雪',
             artist: '刀郎',
-            url: 'https://video.aliang.link/d/云上音乐/01. 2002年的第一场雪.flac',
-            cover: 'https://p2.music.126.net/dCMVhVKVSVi9LQ6cBp_rTg==/109951163281535703.jpg?param=130y130',
-            theme: '#46718b'
+            url: 'https://video.aliang.link/d/天翼云/刀郎 - 2002年的第一场雪.flac',
+            cover: 'https://video.aliang.link/d/天翼云/刀郎 - 2002年的第一场雪.jpg',
+            lrc: 'https://video.aliang.link/d/天翼云/刀郎 - 2002年的第一场雪.lrc',
+            theme: '#31c27c'
+        }, {
+            name: '不如 (女声正式版)',
+            artist: '也可',
+            url: 'https://video.aliang.link/d/天翼云/也可 - 不如 (女声正式版).flac',
+            cover: 'https://video.aliang.link/d/天翼云/也可 - 不如 (女声正式版).jpg',
+            lrc: 'https://video.aliang.link/d/天翼云/也可 - 不如 (女声正式版).lrc',
+            theme: '#6739b6'
+        }, {
+            name: '机器铃 砍菜刀',
+            artist: '张卫',
+            url: 'https://video.aliang.link/d/天翼云/张卫 - 机器铃 砍菜刀.flac',
+            cover: 'https://video.aliang.link/d/天翼云/张卫 - 机器铃 砍菜刀.jpg',
+            lrc: 'https://video.aliang.link/d/天翼云/张卫 - 机器铃 砍菜刀.lrc',
+            theme: '#C20C0C'
         }
     ]
 });
